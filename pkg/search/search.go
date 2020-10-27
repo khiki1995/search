898722ff95ed81e7c19c348794e4538e159a254c
khiki1995 <hikmat0495@gmail.com>
2020-10-27 10:24:01 +0500

    fixed

--- pkg/search/search.go
+++ pkg/search/search.go
@@ -43,31 +43,6 @@ func FindTextInFile(phrase string, filename string) []Result {
 	return fileResult
 }
 
-func FindAnyTextInFile(phrase string, filename string) Result {
-	path, err := filepath.Abs(filename)
-	if err != nil {
-		log.Printf("Error with dirict, dir = %v", path)
-	}
-	file, err := os.Open(path)
-	if err != nil {
-		log.Printf("Error with open file! error = %v", err)
-	}
-	buf := make([]byte,4096)
-	read, err := file.Read(buf) 
-	if err != nil {
-		log.Printf("Error in reading file! error = %v", err)
-	}
-	data := string(buf[:read])
-	arrTxt := strings.Split(data,"\n")
-	var result Result
-	for line, str := range arrTxt {
-		if strings.Contains(str, phrase){	
-			return Result{phrase, str , int64(line)+1, int64(strings.Index(str, phrase))+1,}
-		}
-	}
-	return result	
-}
-
 func All(ctx context.Context, phrase string, files []string) <-chan []Result {
 	ch := make(chan []Result)
 	wg := sync.WaitGroup{}
@@ -122,4 +97,19 @@ func Any(ctx context.Context, phrase string, files []string) <-chan Result {
 		close(ch)
 	}()	
 	return ch
+}
+
+func FindAnyTextInFile(phrase, filetext string) (res Result) {
+	temp := strings.Split(filetext, "\n")
+	for i, line := range temp {
+		if strings.Contains(line, phrase) {
+			return Result{
+				Phrase:  phrase,
+				Line:    line,
+				LineNum: int64(i + 1),
+				ColNum:  int64(strings.Index(line, phrase)) + 1,
+			}
+		}
+	}
+	return res
 }
